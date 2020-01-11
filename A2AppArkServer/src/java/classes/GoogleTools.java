8e6a5473746f55ec7c06fa0eff3df8ce13c27345
FDCarmona <fran_9824@hotmail.com>
2020-01-11 16:47:42 +0100

    fnmoain

---
+++ A2AppArkServer/src/java/classes/GoogleTools.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.net.ssl.HttpsURLConnection;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+/**
+ *
+ * @author Alvaro
+ */
+
+public class GoogleTools {
+    private final String DIRECCION = "https://oauth2.googleapis.com/tokeninfo?id_token=";
+    private final String CLIENT_ID="689413569595-19p08rkfoqssceef8jkph4g44rs0kh3l.apps.googleusercontent.com";
+    private final String JSON_GOOGLE_SUB="sub";
+    private final String JSON_GOOGLE_EMAIL="email";
+    private final String JSON_GOOGLE_AUD="aud";
+    
+    
+    public String registroGoogle(String idGoogleToken){
+        
+        /*
+        Este procedimiento se utilizará para comprobar si el id_token, está verificado
+        En caso de que se produzca alguna exceción este método devolvera null;
+        En caso de que no se haya recibido como respuesta un HTTP_OK (200) o 
+        el aud del json que devuelve, no es igual que el Client_ID devolverá un string vacío
+        En el caso de que todo haya ido correctamente devolverá el idGoogle 
+        */
+        try {
+            String idGoogle="";
+            URL url = new URL(DIRECCION+idGoogleToken);
+            HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+            
+            int responseCode= con.getResponseCode();
+            if(responseCode == HttpsURLConnection.HTTP_OK){
+                //Leer el body de la respuesta 
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine = "";
+                StringBuffer response = new StringBuffer();
+                while((inputLine=in.readLine())!=null){
+                    response.append(inputLine);
+                }
+                in.close();
+            /*---------------------------------------------------------------------------------------------*/
+                
+                JSONObject myResponse = new JSONObject (response.toString());
+                if(myResponse.getString(this.JSON_GOOGLE_AUD).equals(this.CLIENT_ID)){
+                    idGoogle = myResponse.getString(this.JSON_GOOGLE_SUB);
+                    
+                }
+                
+                
+            }
+            
+            
+            con.disconnect();
+            return idGoogle;
+            
+                   
+            
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(GoogleTools.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(GoogleTools.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (JSONException ex) {
+            Logger.getLogger(GoogleTools.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+       return null;
+    }
+    
+    
+    
+    public GoogleTools() {
+    }
+    
+}
---
+++ A2AppArkServer/src/java/entity/Users.java
@@ -0,0 +1,264 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+
+/**
+ *
+ * @author kirek
+ */
+@Entity
+@Table(name = "users")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Users.findAll", query = "SELECT u FROM Users u")
+    , @NamedQuery(name = "Users.findById", query = "SELECT u FROM Users u WHERE u.id = :id")
+    , @NamedQuery(name = "Users.findByName", query = "SELECT u FROM Users u WHERE u.name = :name")
+    , @NamedQuery(name = "Users.findBySurname1", query = "SELECT u FROM Users u WHERE u.surname1 = :surname1")
+    , @NamedQuery(name = "Users.findBySurname2", query = "SELECT u FROM Users u WHERE u.surname2 = :surname2")
+    , @NamedQuery(name = "Users.findByBirthDate", query = "SELECT u FROM Users u WHERE u.birthDate = :birthDate")
+    , @NamedQuery(name = "Users.findByGoogleUser", query = "SELECT u FROM Users u WHERE u.googleUser = :googleUser")
+    , @NamedQuery(name = "Users.findByGoogleId", query = "SELECT u FROM Users u WHERE u.googleId = :googleId")
+    , @NamedQuery(name = "Users.findByEmail", query = "SELECT u FROM Users u WHERE u.email = :email")
+    , @NamedQuery(name = "Users.findByPassword", query = "SELECT u FROM Users u WHERE u.password = :password")
+    , @NamedQuery(name = "Users.findByAdmin", query = "SELECT u FROM Users u WHERE u.admin = :admin")})
+public class Users implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "NAME")
+    private String name;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "SURNAME_1")
+    private String surname1;
+    @Size(max = 45)
+    @Column(name = "SURNAME_2")
+    private String surname2;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "BIRTH_DATE")
+    @Temporal(TemporalType.DATE)
+    private Date birthDate;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "GOOGLE_USER")
+    private short googleUser;
+    @Size(max = 30)
+    @Column(name = "GOOGLE_ID")
+    private String googleId;
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    @Size(max = 45)
+    @Column(name = "EMAIL")
+    private String email;
+    @Size(max = 45)
+    @Column(name = "PASSWORD")
+    private String password;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ADMIN")
+    private short admin;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "users")
+    private Collection<Usefulness> usefulnessCollection;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "reviewer")
+    private Collection<Reviews> reviewsCollection;
+    @JoinColumn(name = "VEHICLE", referencedColumnName = "ID")
+    @ManyToOne
+    private Vehicles vehicle;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Transient
+    protected String googleToken;
+    
+    public Users() {
+    }
+
+    public Users(Integer id) {
+        this.id = id;
+    }
+
+    public Users(Integer id, String name, String surname1, Date birthDate, short googleUser, short admin) {
+        this.id = id;
+        this.name = name;
+        this.surname1 = surname1;
+        this.birthDate = birthDate;
+        this.googleUser = googleUser;
+        this.admin = admin;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname1() {
+        return surname1;
+    }
+
+    public void setSurname1(String surname1) {
+        this.surname1 = surname1;
+    }
+
+    public String getSurname2() {
+        return surname2;
+    }
+
+    public void setSurname2(String surname2) {
+        this.surname2 = surname2;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public short getGoogleUser() {
+        return googleUser;
+    }
+
+    public void setGoogleUser(short googleUser) {
+        this.googleUser = googleUser;
+    }
+
+    public String getGoogleId() {
+        return googleId;
+    }
+
+    public void setGoogleId(String googleId) {
+        this.googleId = googleId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public short getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(short admin) {
+        this.admin = admin;
+    }
+    
+    
+    public String getGoogleToken() {
+        return googleToken;
+    }
+
+    public void setGoogleToken(String googleToken) {
+        this.googleToken = googleToken;
+    }
+
+    @XmlTransient
+    public Collection<Usefulness> getUsefulnessCollection() {
+        return usefulnessCollection;
+    }
+
+    public void setUsefulnessCollection(Collection<Usefulness> usefulnessCollection) {
+        this.usefulnessCollection = usefulnessCollection;
+    }
+
+    @XmlTransient
+    public Collection<Reviews> getReviewsCollection() {
+        return reviewsCollection;
+    }
+
+    public void setReviewsCollection(Collection<Reviews> reviewsCollection) {
+        this.reviewsCollection = reviewsCollection;
+    }
+
+    public Vehicles getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicles vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Users)) {
+            return false;
+        }
+        Users other = (Users) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Users[ id=" + id + " ]";
+    }
+    
+}
