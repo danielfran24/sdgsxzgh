aab9dd1bc122ae5a90c64139e1a5dc469d4ef196
FDCarmona <fran_9824@hotmail.com>
2020-01-14 21:29:05 +0100

    fmioanm

--- AppArkServerMaven/target/generated-sources/annotations/entity/Reviews_.java
+++ AppArkServerMaven/target/generated-sources/annotations/entity/Reviews_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-14T15:55:50")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-14T16:01:08")
 @StaticMetamodel(Reviews.class)
 public class Reviews_ { 
 
--- AppArkServerMaven/target/generated-sources/annotations/entity/Usefulness_.java
+++ AppArkServerMaven/target/generated-sources/annotations/entity/Usefulness_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-14T15:55:50")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-14T16:01:08")
 @StaticMetamodel(Usefulness.class)
 public class Usefulness_ { 
 
